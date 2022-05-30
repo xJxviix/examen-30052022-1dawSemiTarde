d0b5d0fa806c9f8ca8576b607d4310bd2788df0e
xJxviix <jxvi21@icloud.com>
2022-05-30 20:34:25 +0200

    Añadido titulo y label para mostrar la Pregunta

---
+++ JavierSuarezHuacon24/Jugar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JavierSuarezHuacon24
+{
+    public partial class Jugar : Form
+    {
+        public Jugar()
+        {
+            InitializeComponent();
+        }
+    }
+}
