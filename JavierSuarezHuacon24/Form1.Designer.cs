4493c5d1dc6131165a98bde3c9738eaf020ee7f0
xJxviix <jxvi21@icloud.com>
2022-05-30 20:13:35 +0200

    Añadido label y textboxt para introducir datos del jugador

--- JavierSuarezHuacon24/Form1.Designer.cs
+++ JavierSuarezHuacon24/Form1.Designer.cs
@@ -31,6 +31,10 @@ namespace JavierSuarezHuacon24
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.labelPuntuacion = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,11 +48,50 @@ namespace JavierSuarezHuacon24
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(381, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Jugador:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(381, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Puntuación:";
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(507, 66);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(221, 26);
+            this.textBoxNombre.TabIndex = 3;
+            // 
+            // labelPuntuacion
+            // 
+            this.labelPuntuacion.AutoSize = true;
+            this.labelPuntuacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPuntuacion.Location = new System.Drawing.Point(579, 176);
+            this.labelPuntuacion.Name = "labelPuntuacion";
+            this.labelPuntuacion.Size = new System.Drawing.Size(19, 20);
+            this.labelPuntuacion.TabIndex = 4;
+            this.labelPuntuacion.Text = "0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(770, 606);
+            this.Controls.Add(this.labelPuntuacion);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Hola Soy Mister Question";
@@ -61,6 +104,10 @@ namespace JavierSuarezHuacon24
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.Label labelPuntuacion;
     }
 }
 
